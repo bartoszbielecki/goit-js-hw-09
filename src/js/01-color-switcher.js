1a72ae0cf05804873ef4c42749acd7ae8e132ef9
Bartosz Bielecki <bartosz.bielecki@outlook.com>
2024-05-03 17:11:43 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+
+  if (startButton && stopButton) {
+    let intervalId;
+
+    startButton.addEventListener('click', function () {
+      intervalId = setInterval(function () {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+
+      this.disabled = true;
+      stopButton.disabled = false;
+    });
+
+    stopButton.addEventListener('click', function () {
+      clearInterval(intervalId);
+      startButton.disabled = false;
+      this.disabled = true;
+    });
+  } else {
+    console.error('One or both buttons not found.');
+  }
+});
